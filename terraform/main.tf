c46c5347ab6370a9d45b3f0ac08ef283f60ff49a
Mathis Laroche <ecomath360@gmail.com>
2025-03-15 11:26:40 -0400

    Fixed db creation

--- terraform/main.tf
+++ terraform/main.tf
@@ -117,7 +117,7 @@ resource "azurerm_mssql_server" "sqlsrv" {
   resource_group_name          = azurerm_resource_group.rg.name
   location                     = azurerm_resource_group.rg.location
   version                      = "12.0"
-  administrator_login          = "admin"
+  administrator_login          = "my_admin_login"
   administrator_login_password = "password"
 
   minimum_tls_version = "1.2"
